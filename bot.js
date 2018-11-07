52772c6286232eef723cddca6a307654c40047d5
jxco <43864779+jxco@users.noreply.github.com>
2018-11-08 02:06:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,26 +2,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
- console.log("welcome bot"); 
-console.log("log");
+  console.log(Logged in as ${client.user.tag}!);  
 });
 
 client.on('ready', async() => {
-var server = "509779507499237387"; 
-var channel = "509779508044365824";
+var server = "Id server"; 509779507499237387
+var channel = "id room"; 509779508044365824
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('JXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousefJXCOyousef')
+    client.guilds.get(server).channels.get(channel).send('Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , ')
     },305);
 })
 
-
-client.on('message', message => {
-    if(message.content.startsWith('!say')) {
-    if(message.author.id !== "302536427235704832") return;
-    var args = message.content.split(' ').slice(1).join(' ');
-    message.channel.send(args);
-    }
-    });
-
-
-client.login(process.env.BOT_TOKEN);
+client.login("NTA5NzkwMDMyMjQ5MzU2MzA5.DsS6yw.vqBP9Jx6NJBBad8u-JYVG1UmJhA");
